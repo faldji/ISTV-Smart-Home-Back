d32d1a4622afa9a74187319ef41f69fa859aef0c
faldji <fadialamessi@gmail.com>
2020-02-11 10:22:13 +0100

    commit initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# ISTV-Smart-Home-Back
+Une Api rest des fonctionalités de l'application de gestion d'une maison connectée "ISTV Smart Home".
---
+++ src/test/java/istv/smartHome/SmartHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package istv.smartHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
